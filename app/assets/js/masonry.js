de14243b1c4d6ccdb28a964a57c025d275a1e957
john <aarontu803@gmail.com>
2022-09-12 22:47:37 +0800

    修正

--- app/assets/js/masonry.js
+++ app/assets/js/masonry.js
@@ -1,10 +1,22 @@
 let artWork = document.querySelector("#art-work");
+
 let msnry = new Masonry(artWork, {
   percentPosition: true,
   horizontalOrder: true,
 });
 
+let msInit = function () {
+  let msnry = new Masonry(artWork, {
+    percentPosition: true,
+    horizontalOrder: true,
+  });
+};
+
+let collectionTab = document.querySelector("#collection-tab");
+if (collectionTab) {
+  collectionTab.addEventListener("shown.bs.tab", msInit);
+}
+
 imagesLoaded(artWork).on("progress", function () {
-  // layout Masonry after each image loads
   msnry.layout();
 });
